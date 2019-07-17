8d8ed22f219bf98419d5b79557138cf486e5cc32
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2019-07-17 13:34:22 +0300

    SplayTree.h added

---
+++ Files/SplayTree.h
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template <typename T1, typename T2>
+class SplayTree
+{
+	template <typename T1, typename T2>
+	class Node
+	{
+	public:
+		T1 key;
+		T2 data;
+		Node<T1, T2> *leftChild;
+		Node<T1, T2> *rightChild;
+		Node(T1 key = T1(), T2 data = T2(), Node<T1, T2> *leftChild = nullptr, Node<T1, T2> *rightChild = nullptr)
+		{
+			this->key = key;
+			this->data = data;
+			this->leftChild = leftChild;
+			this->rightChild = rightChild;
+		}
+	};
+	Node<T1, T2> *root;
+public:
+	SplayTree();
+};
+
+template<typename T1, typename T2>
+SplayTree<T1, T2>::SplayTree()
+{
+	root = nullptr;
+}
+
+
