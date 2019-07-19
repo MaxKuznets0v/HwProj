1c815c3c84b558075c1542db35f7d8c83114c2eb
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2019-07-19 15:42:34 +0300

    check previous commit description

--- Files/SplayTree.h
+++ Files/SplayTree.h
@@ -2,70 +2,41 @@
 using namespace std;
 
 template <typename T1, typename T2>
-class SplayTree
+class SplayTree 
 {
-template <typename T1, typename T2>
-class Node
-{
-public:
-	T1 key;
-	T2 data;
-	Node<T1, T2> *leftChild;
-	Node<T1, T2> *rightChild;
-	Node(T1 key = T1(), T2 data = T2(), Node<T1, T2> *leftChild = nullptr, Node<T1, T2> *rightChild = nullptr)
+	template <typename T1, typename T2>
+	class Node
 	{
-		this->key = key;
-		this->data = data;
-		this->leftChild = leftChild;
-		this->rightChild = rightChild;
-	}
-};
-Node<T1, T2> *root;
+	public:
+		T1 key;
+		T2 data;
+		Node<T1, T2> *leftChild;
+		Node<T1, T2> *rightChild;
+		Node<T1, T2> *parent;
+		Node(T1 key = T1(), T2 data = T2(), Node<T1, T2> *parent = nullptr, Node<T1, T2> *leftChild = nullptr, Node<T1, T2> *rightChild = nullptr)
+		{
+			this->key = key;
+			this->data = data;
+			this->leftChild = leftChild;
+			this->rightChild = rightChild;
+			this->parent = parent;
+		}
+	};
+	Node<T1, T2> *root;
 public:
 	SplayTree();
 	~SplayTree();
-	Node<T1, T2> *Parent(Node<T1, T2> *node)
+	inline Node<T1, T2> *Parent(Node<T1, T2> *node)
 	{
-		if (node == root)
+		if (node != root && node != nullptr)
+			return node->parent;
+		else
 			return nullptr;
-		Node<T1, T2> *current = root;
-		while (true)
-		{
-			if (node->key < current->key)
-			{
-				if (current->leftChild == node)
-					return current;
-				if (current->leftChild != nullptr)
-					current = current->leftChild;
-				else
-					return nullptr;
-			}
-			else if (node->key > current->key)
-			{
-				if (current->rightChild == node)
-					return current;
-				if (current->rightChild != nullptr)
-					current = current->rightChild;
-				else
-					return nullptr;
-			}
-			else
-			{
-				if (current->rightChild == node)
-					return current;
-				if (current->leftChild == node)
-					return current;
-				if (current->rightChild != nullptr)
-					current = current->rightChild;
-				else
-					return nullptr;
-			}
-		}
 	}
 	void Add(T1 key, T2 elem);
-	void Add(Node<T1, T2> *&node, T1 key, T2 elem);
-	void RotateLeft(Node<T1, T2> *&node);
+	void Add(Node<T1, T2> *&node, Node<T1, T2> *parent, T1 key, T2 elem);
 	void RotateRight(Node<T1, T2> *&node);
+	void RotateLeft(Node<T1, T2> *&node);
 	void Splay(Node<T1, T2> *node);
 };
 
@@ -87,80 +58,143 @@ void SplayTree<T1, T2>::Add(T1 key, T2 elem)
 	if (root == nullptr)
 		root = new Node<T1, T2>(key, elem);
 	else
-		Add(root, key, elem);
+		Add(root, nullptr, key, elem);
 }
 
 template <typename T1, typename T2>
-void SplayTree<T1, T2>::Add(Node<T1, T2> *&node, T1 key, T2 elem)
+void SplayTree<T1, T2>::Add(Node<T1, T2> *&node, Node<T1, T2> *parent,T1 key, T2 elem)
 {
 	if (node == nullptr)
 	{
-		node = new Node<T1, T2>(key, elem);
-		Splay(node);
+		node = new Node<T1, T2>(key, elem, parent);
+		Splay(node); //bring new node to the root
 	}
 	else if (key < node->key)
-		Add(node->leftChild, key, elem);
+		Add(node->leftChild, node, key, elem);
+	else if (key > node->key)
+		Add(node->rightChild, node, key, elem);
 	else
-		Add(node->rightChild, key, elem);
-}
-
-template<typename T1, typename T2>
-void SplayTree<T1, T2>::RotateLeft(Node<T1, T2>* &node)
-{
-	Node<T1, T2> *temp = node->rightChild;
-	node->rightChild = temp->leftChild;
-	temp->leftChild = node;
-	node = temp;
+	{
+		node->data = elem;
+		Splay(node);
+		return;
+	}
 }
 
 template<typename T1, typename T2>
 void SplayTree<T1, T2>::RotateRight(Node<T1, T2>* &node)
 {
-	Node<T1, T2> *temp = node->leftChild;
+	if (node->leftChild == nullptr)
+		return;
+	Node<T1, T2> *temp = node->leftChild; // changing pointers
 	node->leftChild = temp->rightChild;
 	temp->rightChild = node;
-	node = temp;
+	if (node->parent == nullptr)// -------------------changing parents
+	{
+		node->parent = temp;
+		temp->parent = nullptr;
+	}
+	else
+	{
+		temp->parent = node->parent;
+		node->parent = temp;
+	}
+	if (node != root) 
+	{
+		if (node->leftChild != nullptr)
+			node->leftChild->parent = node;
+		node = temp; // now node its child
+		if (node->key > root->key) // depends on in which part (left or right relatively to the root) is node
+			node->parent->rightChild = node;
+		else
+			node->parent->leftChild = node;
+	}
+	else
+	{
+		if (temp->rightChild->leftChild != nullptr)
+			temp->rightChild->leftChild->parent = temp->rightChild;
+		root = temp; //now node its child
+	}
+	// -------------------changing parents
+}
+
+template<typename T1, typename T2>
+void SplayTree<T1, T2>::RotateLeft(Node<T1, T2>*& node)
+{
+	if (node->rightChild == nullptr)
+		return;
+	Node<T1, T2> *temp = node->rightChild; //changing pointers
+	node->rightChild = temp->leftChild;
+	temp->leftChild = node;
+	if (node->parent == nullptr) // -------------------changing parents
+	{
+		node->parent = temp;
+		temp->parent = nullptr;
+	}
+	else
+	{
+		temp->parent = node->parent;
+		node->parent = temp;
+	}
+	if (node != root) 
+	{
+		if (node->rightChild != nullptr)
+			node->rightChild->parent = node;
+		node = temp; //now node its child
+		if (node->key < root->key) // depends on in which part (left or right relatively to the root) is node
+			node->parent->leftChild = node;
+		else
+			node->parent->rightChild = node;
+	}
+	else
+	{
+		if (temp->leftChild->rightChild != nullptr)
+			temp->leftChild->rightChild->parent = temp->leftChild;
+		root = temp; //now node its child
+	}
+	// -------------------changing parents
 }
 
 template<typename T1, typename T2>
 void SplayTree<T1, T2>::Splay(Node<T1, T2>* node)
 {
-	while (Parent(node) != root)
+	while (Parent(node) != nullptr)
 	{
-		Node<T1, T2> *parent = Parent(node);
-		Node<T1, T2> *gparent = Parent(parent);
-		if (parent->leftChild == node)
+		Node<T1, T2>* parent = Parent(node);
+		Node<T1, T2>* gparent = Parent(parent);
+		if (node == parent->leftChild)
 		{
 			if (gparent == nullptr)
-				RotateRight(node);
-			else if (gparent->leftChild == parent)
+				RotateRight(parent); //Zig
+			else if (parent == gparent->leftChild)
 			{
-				RotateRight(gparent);
+				RotateRight(gparent); //Zig-Zig
 				RotateRight(parent);
 			}
 			else
 			{
-				RotateRight(parent);
+				RotateRight(parent); //Zig-Zag
 				RotateLeft(gparent);
 			}
 		}
 		else
 		{
 			if (gparent == nullptr)
-				RotateLeft(node);
-			else if (gparent->rightChild == parent)
+				RotateLeft(parent); //Zig
+			else if (parent == gparent->rightChild)
 			{
-				RotateLeft(gparent);
+				RotateLeft(gparent); //Zig-Zig
 				RotateLeft(parent);
 			}
 			else
 			{
-				RotateLeft(parent);
+				RotateLeft(parent); //Zig-Zag
 				RotateRight(gparent);
 			}
 		}
 	}
-
 }
+
+
 
 
