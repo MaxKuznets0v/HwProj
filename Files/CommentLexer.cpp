7d1bf1d5b70c55efe4155ccc30e3b5869467b5c8
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2019-07-27 14:05:08 +0300

    Finished comment lexer, and inputs updated

--- Files/CommentLexer.cpp
+++ Files/CommentLexer.cpp
@@ -5,20 +5,92 @@ using namespace std;
 
 class CommentLexer
 {
-	enum States
+	enum Move
 	{
-		idle, // idle till doesn't get'/' 
-		open, // after that becomes open and wait for '*'
-		comment, // then text till '*'
-		close, // until gets '/' 
-		other // other symbols
+		slash, // "/" = 0
+		star, // "*" = 1
+		otherSign // "other" = 2
 	};
 	int moveTable[4][3]; //Finite-state machine move table
 	string code; // actual text
 	void GetTable(); // Getting move table from a file
 	void GetCode(); // Getting code from a file
 public:
+	CommentLexer();
+	void FindComments();
 };
+
+CommentLexer::CommentLexer()
+{
+	code = "";
+	GetTable();
+	GetCode();
+}
+
+void CommentLexer::FindComments()
+{
+	int state = 0;
+	Move sign;
+	char prev;
+	char symbol;
+	for (int i = 0; i < code.size(); i++)
+	{
+		symbol = code[i];
+		switch (state)
+		{
+		case 0:
+			if (symbol == '/')
+			{
+				prev = symbol;
+				sign = slash;
+				state = moveTable[state][sign];
+			}
+			break;
+		case 1:
+			if (symbol == '*')
+			{
+				sign = star;
+				cout << prev << symbol;
+				state = moveTable[state][sign];
+			}
+			else if (symbol != '/')
+			{
+				sign = otherSign;
+				state = moveTable[state][sign];
+			}
+			else
+				prev = symbol;
+			break;
+		case 2:
+			if (symbol == '*')
+			{
+				sign = star;
+				prev = symbol;
+				state = moveTable[state][sign];
+			}
+			else
+				cout << symbol;
+			break;
+		case 3:
+			if (symbol == '/')
+			{
+				sign = slash;
+				cout << prev << symbol;
+				state = moveTable[state][sign];
+				cout << endl;
+			}
+			else if (symbol != '*')
+			{
+				sign = otherSign;
+				state = moveTable[state][otherSign];
+				cout << prev << symbol;
+			}
+			else
+				cout << prev;
+			break;
+		}
+	}
+}
 
 void CommentLexer::GetTable()
 {
@@ -42,19 +114,22 @@ void CommentLexer::GetCode()
 {
 	ifstream input;
 	input.open("C:\\Maxim\\Repositories\\HwProj\\inputs\\inputText(CommentLexer).txt");
-	while (input.eof())
+	while (!input.eof())
 	{
 		string temp;
 		getline(input, temp);
-		text += temp;
+		code += temp;
 	}
 	input.close();
 }
 
+
+
 int main()
 {
 	CommentLexer text;
-
+	text.FindComments();
+	
 	system("pause");
 	return 0;
 }
